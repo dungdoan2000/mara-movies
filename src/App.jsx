dbaff925fc4083958d8948229c21f390ad409d6f
dungdoan <dungdoan2000@marahub.com>
2025-09-30 14:10:46 +0700

    commit code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 import Header from './components/Header';
 import Banner from './components/Banner';
 import MovieList from './components/MovieList';
 
 function App() {
+  const [movie, setMovie] = useState([]);
+  const [movieRate, setMovieRate] = useState([]);
+
+  // useEffect này được chạy sau khi component được render lần đầu tiên và mỗi khi dependencies thay đổi. Nếu dependencies là một mảng rỗng, useEffect sẽ chỉ chạy một lần sau khi component được mount. 
+  useEffect(() => {
+    const fechMovie = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
+        }
+      };
+      const urlHotList = 'https://api.themoviedb.org/3/movie/popular?language=vi-US&page=1';
+      const urlTopRate = 'https://api.themoviedb.org/3/movie/top_rated?language=vi-US&page=1';
+
+      const [res1, res2] = await Promise.all([
+        fetch(urlHotList, options),
+        fetch(urlTopRate, options)
+      ])
+      
+      const data1 = await res1.json();
+      const data2 = await res2.json();
+
+      setMovie(data1.results);
+      setMovieRate(data2.results);
+    }
+
+    fechMovie();
+  }, [])
+
   return (
     <>
       <div>
         <Header />
         <Banner />
-        <MovieList title={'Phim hot'}/>
+        <MovieList title={'Phim hot'} data={movie} />
+        <MovieList title={'Phim đề cử'} data={movieRate} />
       </div>
     </>
   )
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 
-const MovieList = ({title}) => {
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 10
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 8
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 3
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
+const MovieList = ({ title, data }) => {
     return (
-        <div className='text-white p-10 mb-10'>{title}</div>
+        <div className='text-white p-10 mb-10'>
+            <h2 className='uppercase text-xl font-bold mb-4'>
+                {title}
+            </h2>
+            <Carousel responsive={responsive} className='flex items-center space-x-4'>
+                {data.length > 0 && data.map((item) => (
+                    <div key={item.id}
+                        className='w-[200px] h-[300px] relative movie-card pr-4 cursor-pointer group overflow-hidden'>
+                        <img
+                            src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`}
+                            alt="img"
+                            className='w-full h-full object-cover rounded-lg
+                            group-hover:scale-110 transition-transform duration-500
+                            ease-in-out'
+                        />
+                        <div className='absolute bottom-0 p-3 bg-black/67 w-full flex justify-center'>
+                            <p className='text-md font-bold'>{item.title || item.orignal_title}</p>
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
+        </div>
     )
 }
 
